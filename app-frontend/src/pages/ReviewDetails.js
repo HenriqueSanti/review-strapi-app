311ed8f0e51e8aba88a36e82fcc0365fab044941
Henrique Santi <henriquesanti@pop-os.localdomain>
2021-10-21 14:49:41 -0400

    ReviewDetails working and Apollo Client added

--- app-frontend/src/pages/ReviewDetails.js
+++ app-frontend/src/pages/ReviewDetails.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
 
 export default function ReviewDetails() {
+    const {id} = useParams()
+    const {loading, error, data} = useFetch('http://localhost:1337/reviews/' + id)
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error :(</p>
+
+    console.log(data)
+
     return (
         <div>
-            Review details
+            <div key={data.id} className="review-card">
+                    <div className="rating">{data.Rating}</div>
+                    <h2>{data.Title}</h2>
+                    <small>Console list</small>
+                    <p>{data.Body}</p>
+                </div>
         </div>
     )
 }
