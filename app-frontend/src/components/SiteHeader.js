437089867e4006fe11fdc08ed02d727fc2b261d7
Henrique Santi <henriquesanti@pop-os.localdomain>
2021-10-22 12:18:01 -0400

    new categories with relational data

--- app-frontend/src/components/SiteHeader.js
+++ app-frontend/src/components/SiteHeader.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { useQuery, gql } from '@apollo/client'
+
+const CATEGORIES = gql`
+    query getCategories {
+        categories {
+            Category,
+            id
+        }
+    }
+`
 
 export default function SiteHeader() {
+    const {loading, error, data} = useQuery(CATEGORIES)
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error :(</p>
+
+    console.log(data)    
+
     return (
         <div className="site-header">
             <Link to="/"><h1>Cinema Reviews</h1></Link>
+            <nav className="categories">
+                {data.categories.map(category => (
+                    <Link key={category.id} to={`/category/${category.id}`}>
+                        {category.Category}
+                    </Link>
+                ))}
+            </nav>
         </div>
     )
 }
